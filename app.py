e1590e3ca43154c7d6b5a6d11563a5b3a9770a1e
vishrutss <vishrutss@gmail.com>
2024-09-15 13:02:41 -0700

    Add routes for starting VM, Stopping VM, and Monitoring VM

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify
+from vm_simulator import start_vm, stop_vm, monitor_vm
+from multiprocessing import Manager
 
 app = Flask(__name__)
 
@@ -6,5 +8,21 @@ app = Flask(__name__)
 def home():
     return jsonify({"message": "Cloud Simulator is running!"})
 
+@app.route('/start_vm/<int:vm_id>', methods=['POST'])
+def start_vm_route(vm_id):
+    message = start_vm(vm_id, vms, manager)
+    return jsonify(message)
+
+@app.route('/stop_vm/<int:vm_id>', methods=['POST'])
+def stop_vm_route(vm_id):
+    message = stop_vm(vm_id, vms)
+    return jsonify(message)
+
+@app.route('/monitor_vm/<int:vm_id>', methods=['GET'])
+def monitor_vm_route(vm_id):
+    return jsonify(monitor_vm(vm_id, vms))
+
 if __name__ == '__main__':
+    manager = Manager()
+    vms = manager.dict()
     app.run(debug=True)
--- vm_simulator.py
+++ vm_simulator.py
@@ -1,13 +1,43 @@
 import time
 import psutil
+from multiprocessing import Process
 
-def simulate_vm():
-    print("VM started")
+def simulate_vm(vm_id, vm_dict):
+    print(f"VM {vm_id} started")
     while True:
         cpu_usage = psutil.cpu_percent(interval=1)
         mem_usage = psutil.virtual_memory().percent
-        print(f"CPU: {cpu_usage}% | Memory: {mem_usage}%")
-        time.sleep(5)
+        vm_dict["cpu"] = cpu_usage
+        vm_dict["memory"] = mem_usage
+        time.sleep(1)
+        
+def start_vm(vm_id, vms, manager):
+    if vm_id not in vms:
+        vm_dict = manager.dict({'pid': None, 'cpu': None, 'memory': None})
+        p=Process(target=simulate_vm, args=(vm_id, vm_dict))
+        p.start()
+        vm_dict['pid'] = p.pid
+        vms[vm_id] = vm_dict
+        return {"message": f"VM {vm_id} started!"}
+    return {"error": f"VM {vm_id} already running!"}
 
-if __name__ == "__main__":
-    simulate_vm()
+def stop_vm(vm_id, vms):
+    if vm_id in vms and vms[vm_id]["pid"] is not None:
+        p = psutil.Process(vms[vm_id]["pid"])
+        p.terminate()
+        p.wait()
+        del vms[vm_id]
+        return {"message": f"VM {vm_id} stopped!"}
+    return {"error": f"VM {vm_id} not found!"}
+
+def monitor_vm(vm_id, vms):
+    if vm_id in vms:
+        vm_dict = vms[vm_id]
+        return {
+            "vm_id": vm_id,
+            "status": "running",
+            "pid": vm_dict["pid"],
+            "cpu": vm_dict["cpu"],
+            "memory": vm_dict["memory"]
+        }
+    return {"error": f"VM {vm_id} not found!"}
